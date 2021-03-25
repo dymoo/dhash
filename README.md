c8b7542fafb7b23caa0ff37721741840628c8422
Dylan Moore <60218243+dymoo@users.noreply.github.com>
2021-03-25 11:58:50 +0000

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# dylan-hash (dhash)
+
+Using sha512 with a random salt to generate a safe password hashing algo.
+
+This code is as non blocking as I can get it in node but probably shouldnt be used in production as I dont really know what I'm doing.
+
+I imagine this is how bcrypt and argon like hashes work but idk 🤷‍♂️
---
+++ index.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const util = require('util');
+
+const randombytesAsync = util.promisify(crypto.randomBytes);
+
+const sha512 = (v) =>
+  crypto.createHash('sha512').update(v, 'utf-8').digest('hex');
+
+const hash = async (password, saltlen = 2) => {
+  const salt = (await randombytesAsync(saltlen)).toString('hex');
+  return `dhash,${saltlen},${sha512(password + salt)}`;
+};
+
+const verify = (hash, password) => {
+  const parsedHash = hash.split(',');
+
+  if (parsedHash.length !== 3 || !parsedHash[0] === 'dhash') {
+    throw Error('Not dhash or malformed hash!');
+  }
+
+  const saltlen = parseInt(parsedHash[1]);
+  const saltspace = Math.pow(saltlen, 16);
+
+  for (var i = 0; i < saltspace; i++) {
+    const salt = i.toString(16);
+    if (parsedHash[2] == sha512(password + salt)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const password = 'hello123';
+
+hash(password).then((hash) => {
+  console.log('hash:', hash);
+  console.log('correct password:', verify(hash, password));
+  console.log('incorrect password:', verify(hash, 'wrong123'));
+});
